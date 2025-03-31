0eb0acf0931bef336b645e544974df60be0033fd
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-03-31 11:27:16 +0200

    Solution

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ module "resource_group_storage" {
   source  = "VolodymyrKaraniuk/resource_group_storage/azurerm"
   version = "1.0.0"
 }
+module "simple_module" {
+  source                = "./modules/simple-module"
+  resource_group_name   = "specific-resource-group"
+  storage_account_name  = "specificstorageaccount"
+  location              = "West US"
+}
